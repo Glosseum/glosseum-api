93a3287f6f383132daa28bd92945cc44c0a7c567
Junseo Kang <karl2003@naver.com>
2023-12-30 22:33:04 +0900

    fix: validation constraint added in logical path

--- endpoint/article/entity.py
+++ endpoint/article/entity.py
@@ -1,4 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, constr, validator, ValidationError, Field, field_validator
+from enum import Enum
+
+
+class ArticleLogic(str, Enum):
+    AGREE = "AGREE"
+    DISAGREE = "DISAGREE"
+    NEUTRAL = "NEUTRAL"
 
 
 class ArticleCreate(BaseModel):
@@ -11,7 +18,7 @@ class ArticleCreate(BaseModel):
 
 class ArticleAppend(BaseModel):
     id: int
-    logic: str = "AGREE"
+    logic: ArticleLogic
     name: str
     content: str
 
@@ -33,7 +40,14 @@ class ArticleGet(BaseModel):
     content: str
     creator_id: int
     path: str
-    path_logical: str
+    path_logical: constr(to_upper=True)
+
+    @field_validator("path_logical")
+    def path_logical_validator(cls, logic: str):
+        for logic in logic.split("/"):
+            if logic not in ArticleLogic.__members__:
+                raise ValidationError(f"{logic}는 잘못된 경로입니다.")
+        return logic
 
     class Config:
         orm_mode = True
